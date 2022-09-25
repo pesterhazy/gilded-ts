8d0eae6af1e74aae89f6eda6247e194e4cb40fb8
Paulus Esterhazy <pesterhazy@gmail.com>
2022-09-25 12:45:15 +0200

    WIP

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,17 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { golden } from '../golden';
+import {readFileSync} from 'fs';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+function capture(f) {
+  let out = "";
+  function print(s) {
+    out += s + "\n";
+  }
+  f(print);
+  return out;
+}
+
+it('Should match expected text', () => {
+  const actual = capture(golden);
+  const expected = readFileSync("test/expected.txt", "utf-8");
+  expect(actual).toBe(expected);
 });
