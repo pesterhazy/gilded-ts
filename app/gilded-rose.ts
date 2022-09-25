e498b4fe576fa687350e9a9b82d213e11fe0ec27
Paulus Esterhazy <pesterhazy@gmail.com>
2022-09-25 13:14:01 +0200

    WIP

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -24,14 +24,6 @@ export class GildedRose {
   }
 }
 
-function inc(item: Item) {
-  updateQuality(item, 1);
-}
-
-function dec(item: Item) {
-  updateQuality(item, -1);
-}
-
 function updateQuality(item: Item, delta: number) {
   item.quality = Math.max(Math.min(item.quality + delta, 50), 0);
 }
@@ -42,17 +34,18 @@ function updateOne(item: Item) {
     item.name != "Backstage passes to a TAFKAL80ETC concert"
   ) {
     if (item.name != "Sulfuras, Hand of Ragnaros") {
-      dec(item);
+      updateQuality(item, -1);
     }
   } else {
+    // TODO what about this?
     if (item.quality < 50) {
       item.quality = item.quality + 1;
       if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
         if (item.sellIn < 11) {
-          inc(item);
+          updateQuality(item, 1);
         }
         if (item.sellIn < 6) {
-          inc(item);
+          updateQuality(item, 1);
         }
       }
     }
@@ -62,11 +55,11 @@ function updateOne(item: Item) {
   }
   if (item.sellIn < 0) {
     if (item.name == "Aged Brie") {
-      inc(item);
+      updateQuality(item, 1);
     } else {
       if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
         if (item.name != "Sulfuras, Hand of Ragnaros") {
-          dec(item);
+          updateQuality(item, -1);
         }
       } else {
         item.quality = 0;
