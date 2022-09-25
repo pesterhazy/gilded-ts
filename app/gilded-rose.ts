1d0c03cbade9bbd2d07c992796b7bd3cbbb406ca
Paulus Esterhazy <pesterhazy@gmail.com>
2022-09-25 13:24:31 +0200

    WIP

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -25,14 +25,21 @@ export class GildedRose {
 }
 
 function updateQuality(item: Item, delta: number) {
-  item.quality = Math.max(Math.min(item.quality + delta, 50), 0);
+  if (!isLegendary(item)) {
+    item.quality = Math.max(Math.min(item.quality + delta, 50), 0);
+  }
+}
+
+function isLegendary(item: Item) {
+  return item.name == "Sulfuras, Hand of Ragnaros";
 }
 
 function updateOne(item: Item) {
-  const newLocal =
+  const specialQuality =
     item.name == "Aged Brie" ||
     item.name == "Backstage passes to a TAFKAL80ETC concert";
-  if (newLocal) {
+
+  if (specialQuality) {
     updateQuality(item, 1);
     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
       if (item.sellIn < 11) {
@@ -43,11 +50,9 @@ function updateOne(item: Item) {
       }
     }
   } else {
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      updateQuality(item, -1);
-    }
+    updateQuality(item, -1);
   }
-  if (item.name != "Sulfuras, Hand of Ragnaros") {
+  if (!isLegendary(item)) {
     item.sellIn = item.sellIn - 1;
   }
   if (item.sellIn < 0) {
@@ -55,9 +60,7 @@ function updateOne(item: Item) {
       updateQuality(item, 1);
     } else {
       if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          updateQuality(item, -1);
-        }
+        updateQuality(item, -1);
       } else {
         item.quality = 0;
       }
