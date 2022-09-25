3be99632d084cc6c436f2ba6a7d1c0ace2430f0d
Paulus Esterhazy <pesterhazy@gmail.com>
2022-09-25 13:04:26 +0200

    WIP

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -25,16 +25,19 @@ export class GildedRose {
 }
 
 function updateOne(item: Item) {
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+  if (
+    item.name != "Aged Brie" &&
+    item.name != "Backstage passes to a TAFKAL80ETC concert"
+  ) {
     if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
         item.quality = item.quality - 1;
       }
     }
   } else {
     if (item.quality < 50) {
       item.quality = item.quality + 1;
-      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
         if (item.sellIn < 11) {
           if (item.quality < 50) {
             item.quality = item.quality + 1;
@@ -48,25 +51,24 @@ function updateOne(item: Item) {
       }
     }
   }
-  if (item.name != 'Sulfuras, Hand of Ragnaros') {
+  if (item.name != "Sulfuras, Hand of Ragnaros") {
     item.sellIn = item.sellIn - 1;
   }
   if (item.sellIn < 0) {
-    if (item.name != 'Aged Brie') {
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (item.name == "Aged Brie") {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    } else {
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
         if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
             item.quality = item.quality - 1;
           }
         }
       } else {
         item.quality = item.quality - item.quality;
       }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
     }
   }
 }
-
